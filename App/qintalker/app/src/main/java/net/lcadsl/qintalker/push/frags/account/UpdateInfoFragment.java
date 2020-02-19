12ac36d7dd390cfb13494f75b6eb092419e89286
lcadsl <815364838@qq.com>
2020-02-19 18:34:31 +0800

    4.29.0相关代码，测试完成头像上传功能

--- App/qintalker/app/src/main/java/net/lcadsl/qintalker/push/frags/account/UpdateInfoFragment.java
+++ App/qintalker/app/src/main/java/net/lcadsl/qintalker/push/frags/account/UpdateInfoFragment.java
@@ -1,11 +1,10 @@
 package net.lcadsl.qintalker.push.frags.account;
 
 
-
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.util.Log;
 
 import com.bumptech.glide.Glide;
 import com.yalantis.ucrop.UCrop;
@@ -13,6 +12,8 @@ import com.yalantis.ucrop.UCrop;
 import net.lcadsl.qintalker.common.app.Application;
 import net.lcadsl.qintalker.common.app.Fragment;
 import net.lcadsl.qintalker.common.widget.PortraitView;
+import net.lcadsl.qintalker.factory.Factory;
+import net.lcadsl.qintalker.factory.net.UploadHelper;
 import net.lcadsl.qintalker.push.R;
 import net.lcadsl.qintalker.push.frags.media.GalleryFragment;
 
@@ -24,7 +25,7 @@ import butterknife.OnClick;
 import static android.app.Activity.RESULT_OK;
 
 /**
- *用户更新信息的界面
+ * 用户更新信息的界面
  */
 public class UpdateInfoFragment extends Fragment {
     @BindView(R.id.im_portrait)
@@ -36,36 +37,34 @@ public class UpdateInfoFragment extends Fragment {
     }
 
 
-
-
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_update_info;
     }
 
     @OnClick(R.id.im_portrait)
-    void onPortraitClick(){
+    void onPortraitClick() {
         new GalleryFragment()
                 .setListener(new GalleryFragment.OnSelectedListener() {
-            @Override
-            public void onSelectedImage(String path) {
-                UCrop.Options options = new UCrop.Options();
-                //设置图片处理的格式
-                options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
-                //设置压缩后的图片精度
-                options.setCompressionQuality(96);
+                    @Override
+                    public void onSelectedImage(String path) {
+                        UCrop.Options options = new UCrop.Options();
+                        //设置图片处理的格式
+                        options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+                        //设置压缩后的图片精度
+                        options.setCompressionQuality(96);
 
-                //得到头像缓存地址
-                File dPath = Application.getPortraitTmpFile();
+                        //得到头像缓存地址
+                        File dPath = Application.getPortraitTmpFile();
 
 
-                UCrop.of(Uri.fromFile(new File(path)),Uri.fromFile(dPath))
-                        .withAspectRatio(1,1)//比例
-                        .withMaxResultSize(520,520)//返回最大的尺寸
-                        .withOptions(options)//相关参数
-                        .start(getActivity());
-            }
-        }).show(getChildFragmentManager(),GalleryFragment.class.getName());
+                        UCrop.of(Uri.fromFile(new File(path)), Uri.fromFile(dPath))
+                                .withAspectRatio(1, 1)//比例
+                                .withMaxResultSize(520, 520)//返回最大的尺寸
+                                .withOptions(options)//相关参数
+                                .start(getActivity());
+                    }
+                }).show(getChildFragmentManager(), GalleryFragment.class.getName());
     }
 
     @Override
@@ -73,7 +72,7 @@ public class UpdateInfoFragment extends Fragment {
         //收到从activity传递过来的回调，然后取出其中的值进行图片加载
         if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
             final Uri resultUri = UCrop.getOutput(data);
-            if (resultUri != null){
+            if (resultUri != null) {
                 loadPortrait(resultUri);
             }
 
@@ -81,13 +80,26 @@ public class UpdateInfoFragment extends Fragment {
             final Throwable cropError = UCrop.getError(data);
         }
     }
+
     //加载uri到当前头像中
-    private void loadPortrait(Uri uri){
+    private void loadPortrait(Uri uri) {
         Glide.with(this)
                 .load(uri)
                 .asBitmap()
                 .centerCrop()
                 .into(mPortrait);
+        //拿到本地文件的地址
+        final String localPath = uri.getPath();
+        Log.e("TAG", "localPath:" + localPath);
+
+        Factory.runOnAsync(new Runnable() {
+            @Override
+            public void run() {
+                String url = UploadHelper.uploadPortrait(localPath);
+                Log.e("TAG", "url:" + url);
+            }
+        });
+
     }
 
 }
--- App/qintalker/factory/src/main/java/net/lcadsl/qintalker/factory/Factory.java
+++ App/qintalker/factory/src/main/java/net/lcadsl/qintalker/factory/Factory.java
@@ -2,10 +2,39 @@ package net.lcadsl.qintalker.factory;
 
 import net.lcadsl.qintalker.common.app.Application;
 
-public class Factory {
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
+public class Factory {
+    //单例模式
+    private static final Factory instance;
+    private final Executor executor;
+
+    static {
+        instance=new Factory();
+    }
+
+    private Factory(){
+        //新建一个四个线程的线程池
+        executor= Executors.newFixedThreadPool(4);
+    }
+
+    /**
+     * 返回全局的Application
+     * @return Application
+     */
     public static Application app(){
-        return null;
+        return Application.getInstance();
+    }
+
+    /**
+     * 异步运行的方法
+     * @param runnable Runnable
+     */
+    //runOnAsync异步
+    public static void runOnAsync(Runnable runnable){
+        //拿到单例拿到线程池然后异步执行
+        instance.executor.execute(runnable);
     }
 
 }
