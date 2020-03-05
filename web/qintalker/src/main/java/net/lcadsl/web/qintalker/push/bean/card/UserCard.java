18c7bd9982ed7c3b127580457a719a17ef5a88ab
lcadsl <815364838@qq.com>
2020-03-05 17:02:32 +0800

    7.6.0，服务器联系人、关注人、搜索、用户信息接口

--- web/qintalker/src/main/java/net/lcadsl/web/qintalker/push/bean/card/UserCard.java
+++ web/qintalker/src/main/java/net/lcadsl/web/qintalker/push/bean/card/UserCard.java
@@ -2,63 +2,72 @@ package net.lcadsl.web.qintalker.push.bean.card;
 
 import com.google.gson.annotations.Expose;
 import net.lcadsl.web.qintalker.push.bean.db.User;
+import net.lcadsl.web.qintalker.push.utils.Hib;
 
 import java.time.LocalDateTime;
 
+/**
+ *
+ * @version 1.0.0
+ */
 public class UserCard {
     @Expose
     private String id;
-
     @Expose
     private String name;
-
     @Expose
     private String phone;
-
     @Expose
     private String portrait;
-
     @Expose
     private String desc;
-
     @Expose
     private int sex = 0;
 
-
-    //用户关注人的数量
+    // 用户关注人的数量
     @Expose
     private int follows;
 
-    //粉丝数量
+    // 用户粉丝的数量
     @Expose
     private int following;
 
-    //我是否已经关注此人
+    // 我与当前User的关系状态，是否已经关注了这个人
     @Expose
     private boolean isFollow;
 
-    //用户信息最后的更新时间
+    // 用户信息最后的更新时间
     @Expose
     private LocalDateTime modifyAt;
 
-
-    public UserCard(final User user){
-        this(user,false);
+    public UserCard(final User user) {
+        this(user, false);
     }
 
+    public UserCard(final User user, boolean isFollow) {
+        this.isFollow = isFollow;
 
-    public UserCard(final User user,boolean isFollow){
-        this.isFollow=isFollow();
-        this.id=user.getId();
-        this.name=user.getName();
-        this.phone=user.getPhone();
-        this.portrait=user.getPortrait();
-        this.desc=user.getDescription();
-        this.sex=user.getSex();
-        this.modifyAt=user.getUpdateAt();
+        this.id = user.getId();
+        this.name = user.getName();
+        this.phone = user.getPhone();
+        this.portrait = user.getPortrait();
+        this.desc = user.getDescription();
+        this.sex = user.getSex();
+        this.modifyAt = user.getUpdateAt();
 
-        //TODO:得到关注人和粉丝的数量
-        //user.getFollowers().size()    会报错，懒加载，因为没有Session
+        // user.getFollowers().size()
+        // 懒加载会报错，因为没有Session
+        Hib.queryOnly(session -> {
+            // 重新加载一次用户信息
+            session.load(user, user.getId());
+            // 这个时候仅仅只是进行了数量查询，并没有查询整个集合
+            // 要查询集合，必须在session存在情况下进行遍历
+            // 或者使用Hibernate.initialize(user.getFollowers());
+            follows = user.getFollowers().size();
+            following = user.getFollowing().size();
+            return null;
+        });
+
     }
 
 
