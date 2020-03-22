19ff87db0a8bd214c7360445bfedd2f7632b496b
lcadsl <815364838@qq.com>
2020-03-22 12:06:40 +0800

    9.18.0，客户端cell模块实现

--- App/qintalker/app/src/main/java/net/lcadsl/qintalker/push/frags/message/ChatFragment.java
+++ App/qintalker/app/src/main/java/net/lcadsl/qintalker/push/frags/message/ChatFragment.java
@@ -1,6 +1,7 @@
 package net.lcadsl.qintalker.push.frags.message;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,18 +11,31 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import net.lcadsl.qintalker.common.app.Fragment;
+import net.lcadsl.qintalker.common.widget.PortraitView;
 import net.lcadsl.qintalker.common.widget.adapter.TextWatcherAdapter;
+import net.lcadsl.qintalker.common.widget.recycler.RecyclerAdapter;
+import net.lcadsl.qintalker.factory.model.db.Message;
+import net.lcadsl.qintalker.factory.model.db.User;
+import net.lcadsl.qintalker.factory.persistence.Account;
 import net.lcadsl.qintalker.push.R;
 import net.lcadsl.qintalker.push.activities.MessageActivity;
+import net.qiujuer.genius.ui.compat.UiCompat;
+import net.qiujuer.genius.ui.widget.Loading;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public abstract class ChatFragment extends Fragment
-implements AppBarLayout.OnOffsetChangedListener {
+        implements AppBarLayout.OnOffsetChangedListener {
     protected String mReceiverId;
+    protected Adapter mAdapter;
 
     @BindView(R.id.toolbar)
     Toolbar mToolBar;
@@ -40,8 +54,6 @@ implements AppBarLayout.OnOffsetChangedListener {
     View mSubmit;
 
 
-
-
     @Override
     protected void initArgs(Bundle bundle) {
         super.initArgs(bundle);
@@ -58,6 +70,8 @@ implements AppBarLayout.OnOffsetChangedListener {
 
         //基本设置
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new Adapter();
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     //初始化toolbar
@@ -80,12 +94,12 @@ implements AppBarLayout.OnOffsetChangedListener {
     }
 
     //初始化输入框监听
-    private void initEditContent(){
+    private void initEditContent() {
         mContent.addTextChangedListener(new TextWatcherAdapter() {
             @Override
             public void afterTextChanged(Editable s) {
-                String content=s.toString().trim();
-                boolean needSendMsg=!TextUtils.isEmpty(content);
+                String content = s.toString().trim();
+                boolean needSendMsg = !TextUtils.isEmpty(content);
                 //设置状态，改变对应icon
                 mSubmit.setActivated(needSendMsg);
             }
@@ -96,27 +110,203 @@ implements AppBarLayout.OnOffsetChangedListener {
     public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
 
     }
+
     @OnClick(R.id.btn_face)
-    void onFaceClick(){
+    void onFaceClick() {
         //TODO
     }
 
     @OnClick(R.id.btn_record)
-    void onRecordClick(){
+    void onRecordClick() {
         //TODO
     }
 
     @OnClick(R.id.btn_submit)
-    void onSubmitClick(){
-        if (mSubmit.isActivated()){
+    void onSubmitClick() {
+        if (mSubmit.isActivated()) {
             //发送
-        }else {
-         onMoreClick();
+        } else {
+            onMoreClick();
         }
     }
 
 
-    private void onMoreClick(){
+    private void onMoreClick() {
         //TODO
+    }
+
+
+    //内容的适配器
+    private class Adapter extends RecyclerAdapter<Message> {
+
+        @Override
+        protected int getItemViewType(int position, Message message) {
+            //自己发送的在右边，收到的在左边
+            boolean isRight = Objects.equals(message.getSender().getId(), Account.getUserId());
+
+
+            switch (message.getType()) {
+                //文字内容
+                case Message.TYPE_STR:
+                    return isRight ? R.layout.cell_chat_text_right : R.layout.cell_chat_text_left;
+
+                //语音内容
+                case Message.TYPE_AUDIO:
+                    return isRight ? R.layout.cell_chat_audio_right : R.layout.cell_chat_audio_left;
+
+                //图片内容
+                case Message.TYPE_PIC:
+                    return isRight ? R.layout.cell_chat_pic_right : R.layout.cell_chat_pic_left;
+
+                //其他内容:
+                default:
+                    return isRight ? R.layout.cell_chat_text_right : R.layout.cell_chat_text_left;
+            }
+        }
+
+        @Override
+        protected ViewHolder<Message> onCreateViewHolder(View root, int viewType) {
+            switch (viewType) {
+                //左右都是同一个
+                case R.layout.cell_chat_text_right:
+                case R.layout.cell_chat_text_left:
+                    return new TextHolder(root);
+
+                case R.layout.cell_chat_audio_right:
+                case R.layout.cell_chat_audio_left:
+                    return new AudioHolder(root);
+
+                case R.layout.cell_chat_pic_right:
+                case R.layout.cell_chat_pic_left:
+                    return new PicHolder(root);
+
+                //默认情况下返回的就是text类型的Holder进行处理
+                //文件的实现
+                default:
+                    return new TextHolder(root);
+            }
+        }
+    }
+
+    /**
+     * Holder的基类
+     */
+    class BaseHolder extends RecyclerAdapter.ViewHolder<Message> {
+        @BindView(R.id.im_portrait)
+        PortraitView mPortrait;
+
+        //允许为空，左边没有右边有
+        @Nullable
+        @BindView(R.id.loading)
+        Loading mLoading;
+
+        public BaseHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            User sender = message.getSender();
+            //进行数据加载
+            sender.load();
+            //头像加载
+            mPortrait.setup(Glide.with(ChatFragment.this), sender);
+
+            //判断当前布局是左是右
+            if (mLoading != null) {
+                //在右边
+                int status = message.getStatus();
+                if (status == Message.STATUS_DONE) {
+                    //正常状态，隐藏loading
+                    mLoading.stop();
+                    mLoading.setVisibility(View.GONE);
+                } else if (status == Message.STATUS_CREATED) {
+                    //正在发送中
+                    mLoading.setVisibility(View.VISIBLE);
+                    mLoading.setProgress(0);
+                    mLoading.setForegroundColor(UiCompat.getColor(getResources(), R.color.colorAccent));
+                    mLoading.start();
+                } else if (status == Message.STATUS_FAILED) {
+                    //发送失败,允许重新发送
+                    mLoading.setVisibility(View.VISIBLE);
+                    mLoading.stop();
+                    mLoading.setProgress(1);
+                    mLoading.setForegroundColor(UiCompat.getColor(getResources(), R.color.alertImportant));
+                }
+
+                mPortrait.setEnabled(status == Message.STATUS_FAILED);
+            }
+        }
+
+
+        @OnClick(R.id.im_portrait)
+        void onRePushClick() {
+            //发送失败后点击头像重新发送
+            if (mLoading != null) {
+                //必须是右边的才能重新发送
+                //TODO 重新发送
+            }
+        }
+    }
+
+
+    /**
+     * 文字的Holder
+     */
+    class TextHolder extends BaseHolder {
+        @BindView(R.id.txt_content)
+        TextView mContent;
+
+
+        public TextHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            super.onBind(message);
+
+            mContent.setText(message.getContent());
+        }
+    }
+
+
+    /**
+     * 语音的Holder
+     */
+    class AudioHolder extends BaseHolder {
+
+
+
+        public AudioHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            super.onBind(message);
+
+            //TODO
+        }
+    }
+
+
+
+    /**
+     * 图片的Holder
+     */
+    class PicHolder extends BaseHolder {
+
+
+
+        public PicHolder(View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        protected void onBind(Message message) {
+            super.onBind(message);
+            //TODO
+        }
     }
 }
